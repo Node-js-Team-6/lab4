e1fe74a5cc215c4e4b97569cca9ce27bb16f8680
Olena Khilchuk <olena.khilchuk@gmail.com>
2021-04-01 16:22:27 +0300

    test2

--- main.js
+++ main.js
@@ -7,13 +7,26 @@ const { User } = require("./classes");
 
 function main() {
     const getId = () => Math.floor(Math.random() * 1000);
+    const services = new Services();
 
     const user = new User({name: 'Vova', id: getId()});
+    const user2 = new User({name: 'Misha', id: getId()});
 
     let root = new Folder({name: 'root', id: getId(), userId: user.id});
-    root.path = Services.getPath(root, root);
+    root.path = services.getPath(root, root);
 
     let folder1 = new Folder({name: 'homevideo', id: getId(), userId: user.id, parentId: root.id});
+    folder1.path = services.getPath(folder1, root);
 
-    let file1 = new File();
+    let file1 = new File({id: getId(), name: 'chm_lab2.py', parentId: root.id, user: user.id, size: '2Kb', downloadCount: 0});
+    file1.path = services.getPath(file1, root);
+
+    let file2 = new File({id: getId(), name: 'smtrpz_kr1.mkv', parentId: folder1.id, user: user.id, size: '5Gb', downloadCount: 3});
+    file2.path = services.getPath(file2, root);
+
+    let file3 = new File({id: getId(), name: 'report_lab2.pdf', parentId: root.id, user: user.id, size: '2Mb', downloadCount: 1});
+    file3.path = services.getPath(file3, root);
+
+    const rating1 = new Rating({id: getId(), userId: user.id, fileId: file2.id, score: 5});
+    const rating2 = new Rating({id: getId(), userId: user2.id, fileId: file2.id, score: 3});
 }
