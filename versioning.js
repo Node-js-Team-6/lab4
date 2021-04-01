0fadefde63d5810c7fd13bffc0a6576f674b4b2b
Olena Khilchuk <olena.khilchuk@gmail.com>
2021-04-01 20:07:09 +0300

    simplified

--- versioning.js
+++ versioning.js
@@ -2,81 +2,53 @@ const zlib = require('zlib');
 const fs = require('fs');
 
 class VersionManager {
-    constructor(dataPath, innerDatafileName, logger = console) {
+    constructor(dataPath, logger = console) {
         this.logger = logger;
-        this.history = [];
-        this.innerDatafileName = innerDatafileName;
-        this.initial = './Versions/';
         this.dataPath = dataPath;
+        this.initial = './Versions';
         this.listeners = []
-        this.history = this.readData(this.innerDatafileName);
     }
 
     saveVersion() {
-        const versionName = Date.now();
 
         fs.readdir(this.dataPath, (err, files) => {
             if(err) {
-                const msg = `${Date.now()}. chchchhchchchc an error occured while reading data from file "${this.dataPath}"\n Error: "${err}"\n`;
+                const msg = `${Date.now()}. an error occured while reading data from file "${this.dataPath}"\n Error: "${err}"\n`;
                 this.logger.log(msg); 
             }
             else {
-                fs.mkdirSync(`${this.initial+versionName}`);
                 files.forEach(filename => {
                     const fileContents = fs.createReadStream(`${this.dataPath}/${filename}`);
-                    const writeStream = fs.createWriteStream(`${this.initial + versionName}/${filename}.gz`);
+                    const writeStream = fs.createWriteStream(`${this.initial}/${filename}.gz`);
                     const zip = zlib.createGzip();
                     fileContents.pipe(zip).pipe(writeStream);
                 })
-        
-                if(!this.history) {
-                    this.history = [];
-                }
-                this.history.push(versionName);
 
-                this.logger.log(`Version '${versionName}' saved\n`);
-                this.writeData(this.innerDatafileName);
+                this.logger.log(`Version saved\n`);
             }
         });		
     }
 
     rollbackVersion() {
-        if (!this.history) {
-            return;
-        }
-
-        const versionName = this.history.pop();
         
-        fs.readdir(this.initial + versionName, (err, files) => {
+        fs.readdir(this.initial, (err, files) => {
             if(err) {
-                const msg = `${Date.now()}. an gggggggggg error occured while reading data from file "${this.initial + versionName}"\n Error: "${err}"\n`;
+                const msg = `${Date.now()}. an error occured while reading data from file "${this.initial}"\n Error: "${err}"\n`;
                 this.logger.log(msg); 
             }
             else {
                 files.forEach(filename => {
-                    const fileContents = fs.createReadStream(`${this.initial + versionName}/${filename}`);
+                    const fileContents = fs.createReadStream(`${this.initial}/${filename}`);
                     const writeStream = fs.createWriteStream(`${this.dataPath}/${filename.slice(0, -3)}`);
                     const unzip = zlib.createGunzip();
                     fileContents.pipe(unzip).pipe(writeStream);
                 });
 
-                this.logger.log(`Version rolled back to ${versionName}\n`);
-                this.writeData(this.innerDatafileName);
+                this.logger.log(`Version rolled back to the saved one\n`);
                 this.listeners.forEach(l => l());
             }
         });	
     }
-
-    readData = (filePath) => JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-
-    writeData = filePath =>
-        new Promise((resolve, reject) => {
-            fs.writeFile(filePath, JSON.stringify(this.history), 'utf-8', () => {
-                const end = Date.now();
-                const msg = `${end}. Data has been written to file "${filePath}"\n`;
-                this.logger.log(msg);
-            })
-        });
 
     subscribe_for_rollback(event_handler)
     {
