6290a88ce200395e6d3dd403e203754973a0864e
Olena Khilchuk <olena.khilchuk@gmail.com>
2021-04-01 16:04:01 +0300

    testing1

--- buisness_logic.js
+++ buisness_logic.js
@@ -6,81 +6,83 @@ const { Rating } = require("./classes");
 const { User } = require("./classes");
 const { VersionManager } = require('./versioning');
 
-const vm = new VersionManager('./database', './versionHistory.json');
-
-let fileRepository = new data.Repositoty('./database/file.json', File.constructor, vm)
-let folderRepository = new data.Repositoty('./database/classes.folder.json', Folder.constructor, vm)
-let userRepository = new data.Repositoty('./database/user.json', User.constructor, vm)
-let ratingRepository = new data.Repositoty('./database/rating.json', Rating.constructor, vm)
-
-
-function addOrUpdateFile(file){
-    fileRepository.addOrUpdate(file)
-}
-
-function deleteFile(file){
-    fileRepository.delete(file)
-}
-
-function addOrUpdateFolder(folder){
-    folderRepository.addOrUpdate(folder)
-}
-
-function deleteFolder(folder){
-    for(let c in folder.children){
-        if(c instanceof Folder){
-            deleteFolder(c)
-        } else {
-            deleteFile(c)
-        }
+class Services {
+    constructor() {
+        const vm = new VersionManager('./database', './versionHistory.json');
+        this.fileRepository = new data.Repositoty('./database/file.json', File.constructor, vm)
+        this.folderRepository = new data.Repositoty('./database/classes.folder.json', Folder.constructor, vm)
+        this.userRepository = new data.Repositoty('./database/user.json', User.constructor, vm)
+        this.ratingRepository = new data.Repositoty('./database/rating.json', Rating.constructor, vm)
     }
-    folderRepository.delete(folder)
+
+    async addOrUpdateFile(file) {
+        await fileRepository.addOrUpdate(file)
+    }
+
+    async deleteFile(file) {
+        await fileRepository.delete(file)
+    }
+
+    async addOrUpdateFolder(folder) {
+        await folderRepository.addOrUpdate(folder)
+    }
+
+    async deleteFolder(folder) {
+        for (let c in folder.children) {
+            if (c instanceof Folder) {
+                deleteFolder(c)
+            } else {
+                deleteFile(c)
+            }
+        }
+
+        await folderRepository.delete(folder)
+    }
+
+    async addOrUpdateUser(user) {
+        await userRepository.addOrUpdate(user)
+    }
+
+    sortByDownloadCount(folder) {
+        folder.children.sort((a, b) => (a instanceof Folder || b instanceof Folder) ? 1 :
+            b.downloadCount - a.downloadCount);
+    }
+
+    sortByRating(folder) {
+        folder.children.sort((a, b) => (a instanceof Folder || b instanceof Folder) ? 1 :
+            b.rating - a.rating);
+    }
+
+    sortByAuthor() {
+        folder.children.sort(function (a, b) {
+            if (a.user.name > b.user.name) return 1;
+            if (a.user.name < b.user.name) return -1;
+            return 0
+        });
+    }
+
+    sortByName(folder) {
+        folder.children.sort(function (a, b) {
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
+            return 0
+        });
+    }
+
+
+    findFileByName(folder, searchText) {
+        return folder.children.filter(file => file.name.startsWith(searchText));
+    }
+
+    findFileByExtension(folder, searchText) {
+        return folder.children.filter(file => !(file instanceof Folder) && file.extension.startsWith(searchText));
+    }
+
+    getPath(element, root) {
+        if (element.id === root.id)
+            return element.name;
+        return getPath(folderRepository.find(element.parentId), root) + '/' + element.name;
+    }
 }
 
-
-function addOrUpdateUser(user){
-    userRepository.addOrUpdate(user)
-}
-
-function sortByDownloadCount(folder)
-{
-    folder.children.sort( (a, b) => (a instanceof Folder || b instanceof Folder) ? 1 :
-        b.downloadCount - a.downloadCount);
-}
-
-function sortByRating(folder)
-{
-    folder.children.sort((a, b) => (a instanceof Folder || b instanceof Folder) ? 1 :
-        b.rating - a.rating);
-}
-
-function sortByAuthor()
-{
-    folder.children.sort(function (a, b) {
-        if (a.user.name > b.user.name) return 1;
-        if (a.user.name < b.user.name) return -1;
-        return 0});
-}
-
-function sortByName(folder)
-{
-    folder.children.sort(function (a, b) {
-        if (a.name > b.name) return 1;
-        if (a.name < b.name) return -1;
-        return 0});
-}
-
-
-function findFileByName(folder, searchText) {
-    return folder.children.filter(file => file.name.startsWith(searchText));
-}
-
-function findFileByExtension(folder, searchText) {
-    return folder.children.filter(file => !(file instanceof Folder) && file.extension.startsWith(searchText));
-}
-
-function getPath(element, root){
-    if (element.id === root.id)
-        return element.name;
-    return getPath(folderRepository.find(element.parentId), root) + '/' + element.name;
-}
+exports.Services = Services;
--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const { Services } = require('./buisness_logic');
+
+const { Folder } = require('./classes.js');
+const { File } = require('./classes.js');
+const { Rating } = require("./classes");
+const { User } = require("./classes");
+
+function main() {
+    const getId = () => Math.floor(Math.random() * 1000);
+
+    const user = new User({name: 'Vova', id: getId()});
+
+    let root = new Folder({name: 'root', id: getId(), userId: user.id});
+    root.path = Services.getPath(root, root);
+
+    let folder1 = new Folder({name: 'homevideo', id: getId(), userId: user.id, parentId: root.id});
+
+    let file1 = new File();
+}
